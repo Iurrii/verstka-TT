3e2a5c5b67d99e90322c83b1e618fb0cfafc64ed
Iurrii <1513it@gmail.com>
2022-02-23 21:54:25 +0100

    сделана сортировка

--- script.js
+++ script.js
@@ -8,45 +8,52 @@ const statistics = [
   },
   {
     name: "Сезон 2020/2019",
-    valueOfGames: 5,
+    valueOfGames: 3,
     goal: 6,
     pass: 2,
     rating: 89.4,
   },
   {
     name: "Сезон 2019/2018",
-    valueOfGames: 5,
+    valueOfGames: 2,
     goal: 5,
     pass: 3,
     rating: 89.4,
   },
   {
     name: "Сезон 2018/2017",
-    valueOfGames: 5,
+    valueOfGames: 4,
     goal: 4,
     pass: 3,
     rating: 89.4,
   },
   {
     name: "Сезон 2017/2016",
-    valueOfGames: 5,
+    valueOfGames: 1,
     goal: 1,
     pass: 4,
     rating: 89.4,
   },
 ];
+let games = document.getElementById("1");
+let goals = document.getElementById("2");
+let pass = document.getElementById("3");
+let goalsEndPass = document.getElementById("4");
+let rating = document.getElementById("5");
+let listnersForTableBtns = [games, goals, pass, goalsEndPass, rating];
 
 let parentForGenerationTable = document.getElementById(
   "table-of-results-output"
 );
 
-console.log(parentForGenerationTable.rows);
-
-createTable(parentForGenerationTable, statistics);
+window.onload = () => {
+  createTable(parentForGenerationTable, statistics);
+  addListenersForTableBtns(listnersForTableBtns);
+};
 
 function createTable(tbody, array) {
   //сделать более универсальную функцию
-  array.forEach(function (item) {
+  array.forEach((item) => {
     let tr = document.createElement("tr");
     let tdname = document.createElement("td");
     let tdvalueOfGames = document.createElement("td");
@@ -72,28 +79,37 @@ function createTable(tbody, array) {
   });
 }
 
-sortTableToLess(parentForGenerationTable);
-
-function sortTableToMore(parent) {
-  //2 сортировка по голам, к большему
+// parent - родитель, куда возвращается отсортированный список строк
+// id - берется из заголовка для каждого типа данных и определяет ячейку в строке
+// trendTo - берётся из кнопки и определяет направление сортировки
+function sortTable(parent, id, trendTo) {
   let arr = Array.from(parent.rows);
-  arr.sort((a, b) => {
-    let str = a.cells[2].textContent;
-    let str2 = b.cells[2].textContent;
-    return str.localeCompare(str2);
-  });
+
+  if (trendTo === "less") {
+    arr.sort((a, b) => {
+      let str = a.cells[id].textContent;
+      let str2 = b.cells[id].textContent;
+      return str2.localeCompare(str);
+    });
+  }
+
+  if (trendTo === "more") {
+    arr.sort((a, b) => {
+      let str = a.cells[id].textContent;
+      let str2 = b.cells[id].textContent;
+      return str.localeCompare(str2);
+    });
+  }
   parent.append(...arr);
 }
 
-function sortTableToLess(parent) {
-  //2 сортировка по голам, к меньшему
-  let arr = Array.from(parent.rows);
-  arr.sort((a, b) => {
-    let str = a.cells[2].textContent;
-    let str2 = b.cells[2].textContent;
-    return str2.localeCompare(str);
+function addListenersForTableBtns(arr) {
+  arr.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      let idParent = e.target.parentElement.id;
+      let trend = e.target.dataset.trend;
+
+      sortTable(parentForGenerationTable, idParent, trend);
+    });
   });
-  parent.append(...arr);
 }
-
-
